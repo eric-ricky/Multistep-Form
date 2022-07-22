23ee16bd394d3b3ab01d42573ef9912490e2c858
Eric Ricky <ericricky200@gmail.com>
2022-07-22 07:14:04 +0300

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import "./App.css";
-import Form from "./components/form";
+import Example from "./components/example";
 
 function App() {
   return (
-    <div className="App">
-      <Form />
+    <div className="grid place-items-center bg-slate-100 h-screen w-full">
+      <div className="container mx-auto md:w-1/2">
+        <div className="flex flex-col items-center gap-4">
+          <h2 className="text-3xl font-bold">Welcome to the platform!</h2>
+          <p className="font-semibold">Follow the steps below to get started</p>
+        </div>
+
+        <div>
+          <Example />
+        </div>
+      </div>
     </div>
   );
 }
